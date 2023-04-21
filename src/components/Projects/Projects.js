ad742e7e8f73367a970a2b017208ec080af85072
lakhan singh rathore <107997584+lakhan0007@users.noreply.github.com>
2023-04-21 23:56:13 +0530

    Clock_description

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -59,7 +59,7 @@ function Projects() {
               imgPath={Clock}
               isBlog={false}
               title="Analog Clock"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
+              description="An analog clock project can also be created using React JS, a popular JavaScript library for building user interfaces. Here's how you can create an analog clock project using React JS:"
               ghLink="https://github.com/soumyajit4419/Editor.io"
               demoLink="https://editor.soumya-jit.tech/"
             />
