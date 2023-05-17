29639f6f655832249cd0faa6e4e2c7918e707b1d
lakhan singh rathore <107997584+lakhan0007@users.noreply.github.com>
2023-05-17 13:45:51 +0530

    updated

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-// import { Container, Row } from "react-bootstrap";
 import Resume1 from "../../Assets/resume/resume1.jpg";
 import Resume2 from "../../Assets/resume/resume2.jpg";
 
@@ -31,18 +30,7 @@ function ResumeNew() {
     <div>
       <Container fluid className="resume-section">
         <Particle />
-        {/* <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-          className="button"
-            variant="primary"
-            style={{ maxWidth: "250px" }}
-            onClick={handleShow}
 
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
-        </Row> */}
 
 
       <Container className="resume-image">
@@ -59,17 +47,10 @@ function ResumeNew() {
 
         
 
-        {/* <Row className="resume">
-          <Document file={resumeLink} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
-        </Row> */}
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            // href={pdf}
-            // target="_blank"
             style={{ maxWidth: "250px" }}
             onClick={handleShow}
           >
