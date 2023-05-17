768b55ae0ae7e3999f7edba8f4d9044106218635
lakhan singh rathore <107997584+lakhan0007@users.noreply.github.com>
2023-05-17 13:50:14 +0530

    gfg

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -17,7 +17,7 @@ function AboutCard() {
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Reading Books
             </li>
             <li className="about-activity">
               <ImPointRight /> Listening Song
